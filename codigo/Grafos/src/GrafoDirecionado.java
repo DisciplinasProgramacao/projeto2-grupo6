73061b61f7b7c00a53c4339bcf9f4ff1e3238bbc
Eric Diniz <64018893+ericdiniz@users.noreply.github.com>
2023-04-03 21:47:28 -0300

    adicionando o grafo direcionado #10

--- codigo/Grafos/src/GrafoDirecionado.java
+++ codigo/Grafos/src/GrafoDirecionado.java
@@ -2,14 +2,22 @@ public class GrafoDirecionado extends GrafoMutavel {
 
 	public GrafoDirecionado(String nome) {
 		super(nome);
-		// TODO Auto-generated constructor stub
 	}
 
-	public boolean addAresta(int origem, int destino, int peso) {
-		return false;
+	@Override
+	public boolean addAresta(int origin, int destiny, int weight) {
+		boolean result = false;
+		Vertice v_origin = this.existeVertice(origin);
+		Vertice v_destiny = this.existeVertice(destiny);
+		if (v_origin != null && v_destiny != null) {
+			result = (v_origin.addAresta(destiny, weight));
+		}
+		return result;
 	}
 
-	public Aresta removAresta(int origem, int destino) {
+	@Override
+	public Aresta removeAresta(int origin, int destiny) {
 		return null;
 	}
+
 }

b7579c22bc898314292a9165b43c42bb43e815b0
Paulo Roncarati <roncarati1209@gmail.com>
2023-04-03 21:22:33 -0300

    Add metodos vazios GrafoNaoDirecionado

--- codigo/Grafos/src/GrafoNaoDirecionado.java
+++ codigo/Grafos/src/GrafoNaoDirecionado.java
@@ -1,3 +1,15 @@
-public class GrafoNaoDirecionado {
-	
+public class GrafoNaoDirecionado extends GrafoMutavel {
+
+	public GrafoNaoDirecionado(String nome) {
+		super(nome);
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean addAresta(int origem, int destino, int peso) {
+		return false;
+	}
+
+	public Aresta removAresta(int origem, int destino) {
+		return null;
+	}
 }

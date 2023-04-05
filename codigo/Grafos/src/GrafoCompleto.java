28a76b8f9a3c0d145560394695ebf4300393c49d
Paulo Roncarati <roncarati1209@gmail.com>
2023-04-04 22:53:18 -0300

    Refatorando GrafoCompleto

--- codigo/Grafos/src/GrafoCompleto.java
+++ codigo/Grafos/src/GrafoCompleto.java
@@ -1,18 +1,13 @@
 public class GrafoCompleto extends Grafo {
 
-    public GrafoCompleto(int ordem) {
-        super("Grafo Completo");
+	public GrafoCompleto(int ordem) {
+		super("Grafo Completo");
 
-        int peso = -1;
+		for (int i = 0; i < ordem; i++)
+			this.vertices.add(i, new Vertice(i));
 
-        for (int i = 0; i < ordem; i++) {
-            this.vertices.add(i, new Vertice(i));
-        }
-
-        Vertice[] listaVertices = new Vertice[vertices.size()];
-
-        for (int i = 0; i < ordem; i++)
-            for (int j = i + 1; j < ordem; j++)
-                addAresta(i, j);
-    }
+		for (int i = 0; i < ordem; i++)
+			for (int j = i + 1; j < ordem; j++)
+				addAresta(i, j);
+	}
 }

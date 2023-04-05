d33fdbc932c78b3af4fe03c73cb096c796e2dd99
Paulo Roncarati <roncarati1209@gmail.com>
2023-04-04 22:50:27 -0300

    Removendo construtor vazio e código comentado

--- codigo/Grafos/src/GrafoCompleto.java
+++ codigo/Grafos/src/GrafoCompleto.java
@@ -1,9 +1,5 @@
 public class GrafoCompleto extends Grafo {
 
-    public GrafoCompleto(String nome) {
-        super(nome);
-    }
-
     public GrafoCompleto(int ordem) {
         super("Grafo Completo");
 
@@ -18,14 +14,5 @@ public class GrafoCompleto extends Grafo {
         for (int i = 0; i < ordem; i++)
             for (int j = i + 1; j < ordem; j++)
                 addAresta(i, j);
-
-        // for (Vertice verticeOrigem : this.vertices.allElements(listaVertices)) {
-        // for (Vertice verticeDestino : this.vertices.allElements(listaVertices)) {
-        // if (verticeOrigem.existeAresta(verticeDestino.getId()) == null) {
-        // this.addAresta(verticeOrigem.getId(), verticeDestino.getId(), peso);
-        // }
-        // }
-        // }
-
     }
 }
